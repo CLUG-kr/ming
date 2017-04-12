8f65b903b86e096bf2c36c78488097ec69ac3df1
JinGyeong Jeong <jjgjoojis@gmail.com>
2017-04-12 16:52:57 +0900

    Get timestamps from speech to text service.

--- src/main/java/Ming/SpeechToTextService.java
+++ src/main/java/Ming/SpeechToTextService.java
@@ -3,6 +3,7 @@ package Ming;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechTimestamp;
 import com.ibm.watson.developer_cloud.speech_to_text.v1.model.Transcript;
 
 import java.io.File;
@@ -21,14 +22,17 @@ public class SpeechToTextService {
         service.setUsernameAndPassword(username, password);
         SpeechResults results = service.recognize(audio, new RecognizeOptions.Builder()
                 .continuous(true)
+                .timestamps(true)
                 .build()).execute();
         List<Transcript> result = results.getResults();
         StringBuilder builder = new StringBuilder();
         for (int j = 0; j < result.size(); j++) {
-            for (int i = 0; i < result.get(j).getAlternatives().size(); i++) {
-                builder.append(i + ": \"");
-                builder.append(result.get(j).getAlternatives().get(i).getTranscript());
-                builder.append("\"\n");
+            builder.append("Transcript 1:\n");
+            Transcript transcript = result.get(j);
+            List<SpeechTimestamp> words = transcript.getAlternatives().get(0).getTimestamps();
+            for (int i = 0; i < words.size(); i++) {
+                builder.append("[" + words.get(i).getStartTime() + " " + words.get(i).getEndTime() + "] ");
+                builder.append(words.get(i).getWord() + "\n");
             }
         }
         return builder.toString();
