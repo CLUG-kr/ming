31d6280e875ad156bdbfe291e64efbf68f0606e4
JinGyeong Jeong <jjgjoojis@gmail.com>
2017-08-28 22:59:55 +0900

    Introduce Sieve interface by moving findLIS

--- src/combiner.ts
+++ src/combiner.ts
@@ -5,46 +5,10 @@ import { RecognitionResult, RecognitionResultWord } from "./data/RecognitionResu
 import { convertSecondsToFormat, normalizeString } from "./utils";
 
 import { LCSMatcher } from "./core/matcher";
+import { LISSieve } from "./core/sieve";
 import { Subtitle } from "./data/Subtitle";
 
 const levenshtein = require('fast-levenshtein');
-
-function findLIS (candidates: Match[]): Match[] {
-        candidates = _.sortBy(candidates, [(candidate: Match) => candidate.endTime]);
-        let lisTable = _.times(candidates.length, _.constant(0));
-        for (let i = 0; i < candidates.length; i++) {
-                const max = candidates
-                        .map((candidate, index) => {
-                                const { startTime, endTime, piece } = candidate;
-                                return { startTime, endTime, piece, index };
-                        })
-                        .filter((candidate) => {
-                                const { startTime, endTime, piece: { id } } = candidate;
-                                return endTime <= candidates[i].startTime && id < candidates[i].piece.id;
-                        })
-                        .map((candidate) => lisTable[candidate.index])
-                        .reduce((a, b) => a > b ? a : b, 0);
-                lisTable[i] = max + 1;
-        }
-        // FIXME: It is possible there are not unique LIS result
-        let nextSequenceLength: number = _.reduce(lisTable, (a, b) => _.max([a, b]));
-        let lastPieceId = 987654321;
-        let lastStartTime = 987654321;
-        let lis = [];
-        for (let i = candidates.length - 1; i >= 0; i--) {
-                if (lisTable[i] !== nextSequenceLength)
-                        continue;
-                else if (candidates[i].piece.id >= lastPieceId)
-                        continue;
-                else if (candidates[i].endTime > lastStartTime)
-                        continue;
-                nextSequenceLength--;
-                lastPieceId = candidates[i].piece.id;
-                lastStartTime = candidates[i].startTime;
-                lis.push(candidates[i]);
-        }
-        return lis.reverse();
-}
 
 export const combine = (subtitle: Subtitle, recognitionResult: RecognitionResult) => {
         return new Promise((resolve, reject) => {
@@ -57,7 +21,7 @@ export const combine = (subtitle: Subtitle, recognitionResult: RecognitionResult
 
                 const candidates = _.flatten(_.map(subtitle.pieces, piece => LCSMatcher(matchContext, piece)));
 
-                const lis = findLIS(candidates);
+                const lis = LISSieve(candidates);
                 lis.forEach(match => {
                         match.piece.setMatch(match);
                 });
---
+++ src/core/sieve.ts
@@ -0,0 +1,45 @@
+import * as _ from "lodash";
+
+import { Match } from "../data/Match";
+
+interface Sieve {
+        (matches: Match[]): Match[];
+}
+
+export const LISSieve = (candidates: Match[]) => {
+        candidates = _.sortBy(candidates, [(candidate: Match) => candidate.endTime]);
+        let lisTable = _.times(candidates.length, _.constant(0));
+        for (let i = 0; i < candidates.length; i++) {
+                const max = candidates
+                        .map((candidate, index) => {
+                                const { startTime, endTime, piece } = candidate;
+                                return { startTime, endTime, piece, index };
+                        })
+                        .filter((candidate) => {
+                                const { startTime, endTime, piece: { id } } = candidate;
+                                return endTime <= candidates[i].startTime && id < candidates[i].piece.id;
+                        })
+                        .map((candidate) => lisTable[candidate.index])
+                        .reduce((a, b) => a > b ? a : b, 0);
+                lisTable[i] = max + 1;
+        }
+        // FIXME: It is possible there are not unique LIS result
+        let nextSequenceLength: number = _.reduce(lisTable, (a, b) => _.max([a, b]));
+        let lastPieceId = 987654321;
+        let lastStartTime = 987654321;
+        let lis = [];
+        for (let i = candidates.length - 1; i >= 0; i--) {
+                if (lisTable[i] !== nextSequenceLength)
+                        continue;
+                else if (candidates[i].piece.id >= lastPieceId)
+                        continue;
+                else if (candidates[i].endTime > lastStartTime)
+                        continue;
+                nextSequenceLength--;
+                lastPieceId = candidates[i].piece.id;
+                lastStartTime = candidates[i].startTime;
+                lis.push(candidates[i]);
+        }
+        return lis.reverse();
+}
+
